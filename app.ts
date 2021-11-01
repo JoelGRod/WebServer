fcb1636ecd83a0391beec9ad692e66aa125e3d16
joelgrod <joelgrod@gmail.com>
2021-11-01 18:57:25 +0000

    Handlebars for views rendering

--- app.ts
+++ app.ts
@@ -1,27 +1,50 @@
-import path from "path";
 import express from "express";
+import path from "path";
+import hbs from "hbs";
+
 import { Request, Response } from "express";
 
 // Global
 const port: number = 8080;
-// Server init
+
+// Server Instance
 const app = express();
 
-// Middleware
-app.use( express.static("public") );
+// View Engine - Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials( path.join(__dirname, '../views/partials') );
+
+// Main routes
+app.get("/", ( req: Request, res: Response ) => {
+    res.render("home", {
+        title: "WebServer",
+        author: "Joel G Rod"
+    });
+});
 
 app.get("/generic", ( req: Request, res: Response ) => {
-    res.sendFile(path.join(__dirname, "../public/generic.html"));
+    res.render("generic", {
+        title: "WebServer",
+        author: "Joel G Rod"
+    });
 });
 
 app.get("/elements", ( req: Request, res: Response ) => {
-    res.sendFile(path.join(__dirname, "../public/elements.html"));
+    res.render("elements", {
+        title: "WebServer",
+        author: "Joel G Rod"
+    });
 });
 
+// Public
+app.use( express.static("public") );
+
+// Extra routes
 app.get("*", ( req: Request, res: Response ) => {
     res.send("<h1>404 - Page Not Found</h1>");
 });
 
+// Server
 app.listen( port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
